ed8213a87de3cf70675b1a08d540c5b0661f1bb2
mkw98 <40563214+mkw98@users.noreply.github.com>
2018-09-22 00:09:34 +0200

    Create program.js

---
+++ program.js
@@ -0,0 +1,22 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        switch (instruction) {
+        	case '/exit':
+            process.stdout.write('Quitting app!\n');
+            process.exit();
+            break;
+        	case 'language':
+        	process.stdout.write(process.env.lang);
+        	break;
+        	case: 'version':
+        	process.stdout.write(process.version);
+			break;
+        	default:
+            process.stderr.write('Wrong instruction!\n');
+        }
+    }
+});
